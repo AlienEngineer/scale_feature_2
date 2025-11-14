45c02e57b3ad02bdee68fefd5f5cd17a63c78aec
Sergio Ferreira <sergio.ferreira@ctw.bmwgroup.com>
2025-11-14 17:02:34 +0000

    removed wrong code

--- feature_2/test/widget_test.dart
+++ feature_2/test/widget_test.dart
@@ -13,14 +13,6 @@ void main() {
     // Build our app and trigger a frame.
     // await tester.pumpWidget(const MyApp());
 
-    var request = ScaleHttpRequest();
-
-    request.addDriveTrain();
-    request.addMobilePlatform();
-    request.addVin();
-
-    var response = await request.get();
-
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
     expect(find.text('1'), findsNothing);
